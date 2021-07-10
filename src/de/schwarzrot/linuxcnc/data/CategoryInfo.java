4c75c1b198c43c927704900329d3ae3862b907be
DjangoReinhard <django013@nobodiesfool.com>
2021-07-10 17:25:22 +0200

    add type to category

--- src/de/schwarzrot/linuxcnc/data/CategoryInfo.java
+++ src/de/schwarzrot/linuxcnc/data/CategoryInfo.java
@@ -1,7 +1,7 @@
 package de.schwarzrot.linuxcnc.data;
-/* 
+/*
  * **************************************************************************
- * 
+ *
  *  file:   	CategoryInfo.java
  *  project:  	GUI for linuxcnc
  *  subproject:	base library for exporthandlers
@@ -9,31 +9,36 @@ package de.schwarzrot.linuxcnc.data;
  *           	will be used to export tooltables from toolmanager
  *  created: 	3.12.2019 by Django Reinhard
  *  copyright:	all rights reserved
- * 
- *  This program is free software: you can redistribute it and/or modify 
- *  it under the terms of the GNU General Public License as published by 
- *  the Free Software Foundation, either version 2 of the License, or 
- *  (at your option) any later version. 
- *   
- *  This program is distributed in the hope that it will be useful, 
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- *  GNU General Public License for more details. 
- *   
- *  You should have received a copy of the GNU General Public License 
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
+ *
  * **************************************************************************
  */
 
 public class CategoryInfo {
    public String getName() {
-      return name;
+      return name == null ? profile : name;
    }
 
 
    public String getProfile() {
       return profile;
+   }
+
+
+   public int getType() {
+      return type;
    }
 
 
@@ -46,6 +51,13 @@ public class CategoryInfo {
       this.profile = profile;
    }
 
+
+   public void setType(int type) {
+      this.type = type;
+   }
+
+
    private String profile;
    private String name;
+   private int    type;
 }
